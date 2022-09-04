73f29c0b6f7347f8cf780f25fb62b4fe67531d13
Peter deHaan <peter@deseloper.com>
2022-09-03 23:47:59 -0700

    Fun w/ excerpts

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    // Optional, default is "---"
+    // excerpt_separator: "<!-- excerpt -->"
+  });
+
+  eleventyConfig.addFilter("inspect", require("node:util").inspect);
+  eleventyConfig.addFilter("keys", function (obj) { return Object.keys(obj) });
 
   return {
     dir: {
